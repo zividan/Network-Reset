2317e0f771775f7fc49a894b21db7df9129e4cc5
Idan Ziv <66304629+zividan@users.noreply.github.com>
2020-10-31 10:38:31 +0200

    Add files via upload

---
+++ Network Reset.bat
@@ -0,0 +1,41 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+@echo off
+title Network Reset
+color 1b
+
+echo Network Reset by: Idan Ziv
+echo #################################
+
+netsh winsock reset
+netsh interface ipv4 reset
+netsh interface ipv6 reset
+ipconfig /flushdns
+
+echo Your computer will be restarted now
+PAUSE
+shutdown /r /t 1
